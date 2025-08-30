adb0156fb399ee65da794b6985e246fa8b9cdd3a
Alplander <73280544+Alplander@users.noreply.github.com>
2025-08-29 18:12:41 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE = "trivia-duel-v2"; // bump to v2
+const CACHE = "trivia-duel-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
-self.addEventListener("install", (e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
-self.addEventListener("activate", (e)=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
-self.addEventListener("fetch", (e)=>{
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)));
+      await self.clients.claim();
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
   const url = new URL(e.request.url);
-  if(url.origin === location.origin){
-    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
   }
 });
